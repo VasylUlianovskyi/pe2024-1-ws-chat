82bbc77156863f83c31ae297ae70ee65c8091c1f
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-11-03 17:09:31 +0200

    Add socket on client and server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 const axiosOptions = {
   baseURL: 'http://127.0.0.1:5000/api',
@@ -10,3 +11,21 @@ export const getMessages = limit => apiInstance.get(`/messages?limit=${limit}`);
 
 export const createMessage = newMessage =>
   apiInstance.post('/messages', newMessage);
+
+//========
+
+const socketClient = io('ws://localhost:5000'); // conection
+
+socketClient.on('HELLO_TO_ME', () => {
+  console.log('Hello in our chat');
+});
+
+socketClient.on('NEW_PARTICIPANT', () => {
+  console.log('New perticipant joined');
+});
+
+socketClient.on('CHAT_WILL_CLOSED', () => {
+  console.log('We will closed in a few minutes');
+});
+
+socketClient.emit('SEND_TO_SERVER', 'Message');
--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
+
+// ---------------
+
+const cors = { origin: '*' };
+
+const wsServer = new Server(httpServer, { cors });
+
+wsServer.on('connection', socket => {
+  console.log('Connection established');
+  socket.emit('HELLO_TO_ME');
+
+  socket.broadcast.emit('NEW_PARTICIPANT');
+
+  wsServer.emit('CHAT_WILL_CLOSED');
+
+  socket.on('SEND_TO_SERVER', message => {
+    console.log('From client:', message);
+  });
+});
+
+//--------------
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
