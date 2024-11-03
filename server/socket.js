7e4a1ea54659c9247f7c89bcd70b3118cb00b4ed
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-11-03 18:59:02 +0200

    move socket events

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,9 @@
 const { Server } = require('socket.io');
+
 const { Message } = require('./models');
+const {
+  SOCKET_SERVER_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
+} = require('./constants');
 
 function initSocket (httpServer) {
   const cors = { origin: '*' };
@@ -9,12 +13,12 @@ function initSocket (httpServer) {
   wsServer.on('connection', socket => {
     console.log('Connection established');
 
-    socket.on('NEW_MESSAGE', async payload => {
+    socket.on(NEW_MESSAGE, async payload => {
       try {
         const createdMessage = await Message.create(payload);
-        wsServer.emit('NEW_MESSAGE_SUCCESS', createdMessage);
+        wsServer.emit(NEW_MESSAGE_SUCCESS, createdMessage);
       } catch (error) {
-        socket.emit('NEW_MESSAGE_ERROR', error);
+        socket.emit(NEW_MESSAGE_ERROR, error);
       }
     });
   });
