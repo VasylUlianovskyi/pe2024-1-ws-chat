03eb2464ae5eaf329d15a91e2ded9468e3a5080a
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-11-03 19:36:40 +0200

    Add scroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { getMessagesThunk } from './store/slices/messagesSlice';
 import styles from './App.module.css';
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { ws } from './api';
 
 function App ({ messages, isFetching, error, limit, create, get }) {
+  const scrollTo = useRef(null);
+
   useEffect(() => {
     get(limit);
   }, [limit]);
@@ -16,9 +18,19 @@ function App ({ messages, isFetching, error, limit, create, get }) {
     formikBag.resetForm();
   };
 
+  useEffect(() => {
+    scrollTo?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
-    <article>
-      <section>
+    <article
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+      }}
+    >
+      <section style={{ overflowY: 'auto' }}>
         <ul>
           {messages.map(m => (
             <li key={m._id} className={styles.messageItem}>
@@ -28,13 +40,13 @@ function App ({ messages, isFetching, error, limit, create, get }) {
             </li>
           ))}
         </ul>
-        <div>
+        <div ref={scrollTo} style={{ height: '20px' }}>
           {error && <div style={{ color: 'red' }}>ERROR!!!</div>}
           {isFetching && <div>Messages is loading. Please, wait...</div>}
         </div>
       </section>
 
-      <section className={styles.formContainer}>
+      <section className={styles.formContainer} style={{ marginTop: 'auto' }}>
         <Formik initialValues={{ body: '' }} onSubmit={addMessage}>
           <Form>
             <Field name='body'></Field>
